2255c548afa1b435feba0091bc051fcd1fb3938e
mitsunobu1201 <87081727+mitsunobu1201@users.noreply.github.com>
2023-02-09 23:52:58 +0000

    [start]4-5

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'lists/new'
   post 'lists' => 'lists#create'
   get 'lists' => 'lists#index'
-  get 'lists/edit'
   get '/top' => 'homes#top'
   get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
 end
